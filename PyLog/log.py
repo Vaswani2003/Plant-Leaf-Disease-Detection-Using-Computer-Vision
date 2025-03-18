50b3fcdf97a2094092c577176094a27cc050ca33
Vinamra Vaswani <vinamravaswani@gmail.com>
2025-03-18 23:49:16 +0530

    Setup FastAPI Server and PyLog

--- PyLog/log.py
+++ PyLog/log.py
@@ -1,12 +1,13 @@
 import os
+from datetime import datetime
 from pydantic import BaseModel
 
 class Log(BaseModel):
-    level: int
+    level: str
     message: str
     module: str
     method_trace: str
-    timestamp: str
+    timestamp: datetime
 
 
 class Logger:
@@ -16,11 +17,11 @@ class Logger:
         self.folder_path = folder_path
         self.filter_level = 0
         self.filters = {
-            0: "INFO",
-            1: "DEBUG",
-            2: "WARNING",
-            3: "ERROR",
-            4: "CRITICAL"
+            "INFO" : 0,
+            "DEBUG" : 1,
+            "WARNING" : 2,
+            "ERROR" : 3,
+            "CRITICAL" : 4
         }
 
         # create a folder if it does not exist
@@ -28,7 +29,7 @@ class Logger:
             os.makedirs(folder_path)
 
     def set_filter_level(self, level):
-        self.filter_level = level.upper()
+        self.filter_level = self.filters[level.upper()]
 
     @staticmethod
     def get_instance():
@@ -39,12 +40,12 @@ class Logger:
   
     def _write_log(self, log: Log):
         #create a file with current date
-        current_date = log.timestamp.split(" ")[0]
+        current_date = log.timestamp.strftime("%d-%m-%Y")
         file_path = f"{self.folder_path}/{current_date}.log"
         with open(file_path, "a") as file:
             file.write(f"{log.timestamp} - {self.filters[log.level]} - Module: {log.module} - Method: {log.method_trace} - Message: {log.message}\n")
     
     def log(self, level, message, module, method_trace, timestamp):
-        if level >= self.filter_level:
+        if self.filters[level.upper()] >= self.filter_level:
             log = Log(level=level, message=message, module=module, method_trace=method_trace, timestamp=timestamp)
             self._write_log(log)
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-pass
+import os
+import sys
+from datetime import datetime
+from fastapi import FastAPI
+from PyLog.log import Logger
+
+project_root = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(project_root)
+
+server = FastAPI()
+
+@server.get("/")
+def handle_get_root():
+    Logger.get_instance().log(
+        level="INFO",
+        module="Main",
+        method_trace="handle_get_root",
+        message="Entered server root",
+        timestamp=datetime.now()
+    )
+    return {"message": "Server Up and Running"}
